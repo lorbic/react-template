2987477c77947bcd732337fc39449dba26290941
Vikash <vikaspatelp83@gmail.com>
2022-07-06 18:31:53 +0530

    Add starter template

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "App.css";
+
+
+const App = () =>  (
+    <div>
+        <h1>Hello React!</h1>
+        <p>This is a template for ReactJs Projects</p>
+    </div>
+);
+
+export default App;
